48183ba83d707520a358106efd49d36e046d3291
lharnim <104135871+lharnim@users.noreply.github.com>
2023-03-01 14:31:05 +0530

    demo

---
+++ swap.c
@@ -0,0 +1,13 @@
+/*this is a demo*/
+
+
+#include<stdio.h>
+void main()
+{
+int i=10,j=20,temp=0;
+printf("before swap: a: %d, b: &d", &a &b);
+temp=a;
+a=b;
+b=temp;
+printf("after swap: a: %d, b: &d", &a &b);
+}
